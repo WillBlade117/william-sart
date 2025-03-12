9d233a4b85d661a3c7ba4c2b200edc272df6a58d
William <sart.william@gmail.com>
2025-03-12 20:01:34 +0100

    animation 3D

--- app/components/Avatar3D.jsx
+++ app/components/Avatar3D.jsx
@@ -2,26 +2,43 @@
 
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
-import { Suspense, useRef } from "react";
+import { Suspense, useRef, useEffect, useState } from "react";
+import * as THREE from "three";
 
 const Avatar3D = () => {
-  const { scene } = useGLTF("/avatar2.glb");
   const avatarRef = useRef();
+  const [currentAnimation, setCurrentAnimation] = useState(0);
+  const { scene: avatarScene } = useGLTF("/avatar.glb");
+  const { animations } = useGLTF("/animationsbis.glb");
+  const mixer = useRef(new THREE.AnimationMixer(avatarScene));
 
-  useFrame(() => {
-    if (avatarRef.current) {
-      avatarRef.current.rotation.y += 0.01;
+  useEffect(() => {
+    if (animations.length > 0) {
+      const action = mixer.current.clipAction(animations[currentAnimation]);
+      mixer.current.stopAllAction();
+      action.play();
     }
+
+    const interval = setInterval(() => {
+      setCurrentAnimation((prev) => (prev + 1) % animations.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [currentAnimation, animations]);
+
+  useFrame((_, delta) => {
+    // La rotation a été supprimée
+    mixer.current.update(delta);
   });
 
-  scene.position.set(0, -7, 0);
+  avatarScene.position.set(0, -7, 0);
 
-  return <primitive ref={avatarRef} object={scene} scale={6} />;
+  return <primitive ref={avatarRef} object={avatarScene} scale={6} />;
 };
 
 const AvatarScene = () => {
   return (
-    <Canvas camera={{ position: [0, 1, 10], fov: 50 }} className="">
+    <Canvas camera={{ position: [0, 1, 10], fov: 50 }}>
       <ambientLight intensity={0.5} />
       <directionalLight position={[2, 2, 2]} />
       <Suspense fallback={null}>
